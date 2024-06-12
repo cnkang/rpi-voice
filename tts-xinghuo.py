82c08e1e37a26ca7950b13f76a23cdbb6bf153bc
Kang Liu <555836+cnkang@users.noreply.github.com>
2024-06-12 18:29:00 +0800

    debug auth issue #1

--- tts-xinghuo.py
+++ tts-xinghuo.py
@@ -1,6 +1,14 @@
 # -*- coding:utf-8 -*-
 #  错误码链接：https://www.xfyun.cn/document/error-code （code返回错误码时必看）
 import websocket
+from dotenv import load_dotenv
+
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import ssl
+
 import datetime
 import hashlib
 import base64
@@ -8,13 +16,13 @@ import hmac
 import json
 from urllib.parse import urlencode
 import time
-import ssl
 from wsgiref.handlers import format_date_time
 from datetime import datetime
 from time import mktime
 import _thread as thread
 import os
 
+websocket.enableTrace(True)
 
 STATUS_FIRST_FRAME = 0  # 第一帧的标识
 STATUS_CONTINUE_FRAME = 1  # 中间帧标识
@@ -45,9 +53,10 @@ class Ws_Param(object):
         date = format_date_time(mktime(now.timetuple()))
 
         # 拼接字符串
-        signature_origin = "host: " + "ws-api.xfyun.cn" + "\n"
+        signature_origin = "host: " + "tts-api.xfyun.cn" + "\n"
         signature_origin += "date: " + date + "\n"
         signature_origin += "GET " + "/v2/tts " + "HTTP/1.1"
+
         # 进行hmac-sha256进行加密
         signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
                                  digestmod=hashlib.sha256).digest()
@@ -60,15 +69,21 @@ class Ws_Param(object):
         v = {
             "authorization": authorization,
             "date": date,
-            "host": "ws-api.xfyun.cn"
+            "host": "tts-api.xfyun.cn"
         }
         # 拼接鉴权参数，生成url
         url = url + '?' + urlencode(v)
-        # print("date: ",date)
-        # print("v: ",v)
+        print("date: ",date)
+        print("v: ",v)
         # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
-        # print('websocket url :', url)
+        print('websocket url :', url)
         return url
+
+load_dotenv()
+APPID=os.getenv('XH_APPID')
+APISecret=os.getenv('XH_APISecret')
+APIKey=os.getenv('XH_APIKey')
+wsParam = Ws_Param(APPID, APISecret,APIKey,Text="这是一个语音合成示例")
 
 def on_message(ws, message):
     try:
@@ -101,8 +116,11 @@ def on_error(ws, error):
 
 
 # 收到websocket关闭的处理
-def on_close(ws):
+def on_close(ws, status_code, close_reason):
     print("### closed ###")
+    print("WebSocket closed with status code:", status_code)
+    print("Close reason:", close_reason)
+
 
 
 # 收到websocket连接建立的处理
@@ -123,9 +141,6 @@ def on_open(ws):
 
 if __name__ == "__main__":
     # 测试时候在此处正确填写相关信息即可运行
-    wsParam = Ws_Param(APPID=' ', APISecret=' ',
-                       APIKey=' ',
-                       Text="这是一个语音合成示例")
     websocket.enableTrace(False)
     wsUrl = wsParam.create_url()
     ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close)
